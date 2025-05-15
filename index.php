fbe58b7be312d4713846798e49ae14b6e4a6572f
crowwraith <1185615@student.roc-nijmegen.nl>
2025-05-15 12:06:13 +0200

    header update
    header skeleton, no styling yet

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
 <?php
+$servername = "mysql_db_2";
+$username = "root";
+$password = "rootpassword";
 
-
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=restaurant_1", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+}
 ?>
 <!doctype html>
 <html lang="en">
@@ -12,6 +20,35 @@
     <title>Document</title>
 </head>
 <body>
+<header>
+<div class="index-header-main">
+    <div class="index-header-topbar">
+        // TODO alleen vliegvakanties, rest optioneel aanbod
+        <div><a href="index.php">vacanties</a></div>
+        <div><a href="index.php">vliegtickets</a></div>
+        <div><a href="index.php">cruises</a></div>
+        <div><a href="index.php">meer reizen</a></div>
+        <p>vind jou ideale vakantie</p>
+    </div>
+    <div class="index-header-searchbars">
+        <div>
+            <div> vakantietype  </div>
+            <div> bestemming    </div>
+            <div> wanneer       </div>
+            <div> wie?          </div>
+        </div>
+        <div>
+            <zoekvelden voor bovenstaande opties. zie databasebeheer>
+        </div>
+        <div>zoeken</div>
+    </div>
+</div>
+</header>
+<main>
+// 5 delen
+</main>
+<footer>
 
+</footer>
 </body>
 </html>
